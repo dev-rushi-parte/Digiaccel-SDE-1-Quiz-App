0521f0f82815c8d1253c27c2b992295046fd9088
rushi2784 <96285307+rushi2784@users.noreply.github.com>
2022-11-21 17:19:32 +0530

    change for chart

--- backend/Routes/ResultAttempt.routes.js
+++ backend/Routes/ResultAttempt.routes.js
@@ -9,17 +9,26 @@ ResultRouter.post("/:id", async (req, res) => {
         const { userId, scrore } = req.body
 
         const checkUser = await ResultModel.findOne({ userId: id });
+        const size = await ResultModel.findOne({ userId: id }).count();
         console.log(id, "frId")
         console.log(checkUser, "userData")
         console.log(userId, "userId")
         if (checkUser) {
             if (checkUser.userId == userId) {
-                console.log(id)
-                let att = await checkUser.updateOne({ $push: { attempt: Number((checkUser.attempt[checkUser.attempt.length - 1])) + 1 } });
-                let scor = await checkUser.updateOne({ $push: { scrore: scrore } });
+                const AddNewUserResult = new ResultModel({
+                    userId,
+                    scrore,
+                    attempt: size + 1
 
+                })
 
-                res.status(200).json({ "message": "Data Updated" })
+                try {
+                    const saveResult = await AddNewUserResult.save();
+                    res.status(201).json(saveResult);
+                }
+                catch (err) {
+                    res.status(500).json(err)
+                }
             }
             else {
                 res.status(401).send("You are Unauthorized")
